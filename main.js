12f8ee4daa05f52c4442b1d2e9f92ab8da8a5127
moataz2002 <mezzo.m.2030@gmail.com>
2024-04-05 15:19:57 +0200

    done

--- main.js
+++ main.js
@@ -21,6 +21,7 @@ async function getNews(){
                     <br>
                     <br>
                     <br>
+                    <br>
                     `
                 console.log(response.results[i]['media'][0].caption);
             }
